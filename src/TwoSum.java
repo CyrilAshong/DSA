c5687abd131df4b318c1af1782017bf08717d568
CyrilAshong <ashongcyril2704@gmail.com>
2024-03-24 16:55:07 +0000

    TwoSum Algorithm

---
+++ src/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[7];
+        System.out.print("Enter target number: ");
+        int target = input.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    int[] num = {arr[i],arr[j]};
+                    System.out.println(Arrays.toString(num));
+                }
+            }
+        }
+    }
+}
