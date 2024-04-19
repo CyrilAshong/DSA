56b4a423da40941f0ca3136b716e06d78536f5dc
CyrilAshong <ashongcyril2704@gmail.com>
2024-04-19 13:21:30 +0000

    TwoSum Modification

--- src/EvenDigits.java
+++ src/EvenDigits.java
@@ -1,45 +1,4 @@
 public class EvenDigits {
-//    public static void main(String[] args) {
-//        int[] nums = {12, 34, 45, 56, 67, 78, 89, 90};
-//
-//        System.out.println(findNumbers(nums));
-//
-//    }
-//
-//    static  int findNumbers(int[] arr) {
-//        int count = 0;
-//
-//        for(int element : arr){
-//            if (even(element)) {
-//                count ++;
-//            }
-//        }
-//
-//        return count;
-//    }
-//
-//    static boolean even(int num) {
-//        int numOfDIgits = digits(num);
-//
-//        if(numOfDIgits % 2 == 0){
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    static int digits(int num) {
-//        int count = 0;
-//
-//        while (num > 0){
-//            count++;
-//
-//            num /= 10;
-//        }
-//        return count;
-//    }
-//
-
-
     public static void main(String[] args) {
         int[] nums = {123, 3, 34, 452, 56, 67, 7843, 8, 906};
 
--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,22 +1,19 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class TwoSum {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[] arr = new int[4];
-        System.out.print("Enter target number: ");
-        int target = input.nextInt();
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 5;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = input.nextInt();
-        }
+        twoSum(arr, target);
+    }
 
-        for (int i = 0; i < arr.length; i++) {
+    static void twoSum(int[] arr, int target){
+        for (int k : arr) {
             for (int j = 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) {
-                    int[] num = {arr[i],arr[j]};
-                    System.out.println(Arrays.toString(num));
+                if (k + arr[j] == target) {
+                    int[] result = {k, arr[j]};
+                    System.out.println(Arrays.toString(result));
                 }
             }
         }
